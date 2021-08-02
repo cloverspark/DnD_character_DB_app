c1956929d8ebc7fedc2d1eeeac330a455b7ad2a8
cloverspark <80292719+cloverspark@users.noreply.github.com>
2021-08-02 08:41:16 -0700

    Add files via upload

---
+++ DnDCharaterDBApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DnDCharaterDBApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
