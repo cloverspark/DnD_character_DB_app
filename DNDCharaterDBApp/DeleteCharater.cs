fa78b82d6eb61596153310650e48a127762a9bd4
cloverspark <alexbrenna1999@hotmail.com>
2021-08-05 14:01:41 -0700

    added update, delete , add forms

---
+++ DNDCharaterDBApp/DeleteCharater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNDCharaterDBApp
+{
+    public partial class DeleteCharater : Form
+    {
+        public DeleteCharater()
+        {
+            InitializeComponent();
+        }
+    }
+}
