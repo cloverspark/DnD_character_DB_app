a97be89a2c28a27ae65a6f6a6fdbbc6a30d2c203
cloverspark <alexbrenna1999@hotmail.com>
2021-08-11 15:05:45 -0700

    madde first get all method for current Characters

--- DNDCharaterDBApp/CharacterSheetDatabase.cs
+++ DNDCharaterDBApp/CharacterSheetDatabase.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
+using System.Data;
 namespace DNDCharaterDBApp
 {
     class CharacterSheetDatabase
     {
+        public static DataTable GetAllSheets()
+        {
+            SqlConnection con = DbHelper.GetConnection();
+
+            SqlDataAdapter da = new SqlDataAdapter("select SheetID, CharterName, RaceName from CharterSheet , Racewhere CharterSheet.race = Race.RaceID", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
     }
 }
