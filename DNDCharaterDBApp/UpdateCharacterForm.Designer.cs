6340a42f217216df541458254e6a34db60aed9f5
Ammon Johnson <ammonsawesome@gmail.com>
2021-08-24 11:52:26 -0700

    Removed description textbox and changed the listbox label and fixed the styling

--- DNDCharaterDBApp/UpdateCharacterForm.Designer.cs
+++ DNDCharaterDBApp/UpdateCharacterForm.Designer.cs
@@ -30,7 +30,6 @@ namespace DNDCharaterDBApp
         private void InitializeComponent()
         {
             this.CharacterListBx = new System.Windows.Forms.ListBox();
-            this.DescriptionTxtBx = new System.Windows.Forms.TextBox();
             this.HairStyleTxtBx = new System.Windows.Forms.TextBox();
             this.HairColorTxtBx = new System.Windows.Forms.TextBox();
             this.EyeColorTxtBx = new System.Windows.Forms.TextBox();
@@ -47,7 +46,6 @@ namespace DNDCharaterDBApp
             this.label8 = new System.Windows.Forms.Label();
             this.CharacterTxtBx = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.UpdateBtn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -64,17 +62,10 @@ namespace DNDCharaterDBApp
             this.CharacterListBx.TabIndex = 0;
             this.CharacterListBx.SelectedIndexChanged += new System.EventHandler(this.CharacterListBx_SelectedIndexChanged);
             // 
-            // DescriptionTxtBx
-            // 
-            this.DescriptionTxtBx.Location = new System.Drawing.Point(801, 478);
-            this.DescriptionTxtBx.Name = "DescriptionTxtBx";
-            this.DescriptionTxtBx.Size = new System.Drawing.Size(323, 22);
-            this.DescriptionTxtBx.TabIndex = 9;
-            // 
             // HairStyleTxtBx
             // 
             this.HairStyleTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HairStyleTxtBx.Location = new System.Drawing.Point(1075, 149);
+            this.HairStyleTxtBx.Location = new System.Drawing.Point(1082, 192);
             this.HairStyleTxtBx.Name = "HairStyleTxtBx";
             this.HairStyleTxtBx.Size = new System.Drawing.Size(222, 24);
             this.HairStyleTxtBx.TabIndex = 2;
@@ -82,7 +73,7 @@ namespace DNDCharaterDBApp
             // HairColorTxtBx
             // 
             this.HairColorTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HairColorTxtBx.Location = new System.Drawing.Point(1075, 229);
+            this.HairColorTxtBx.Location = new System.Drawing.Point(1082, 282);
             this.HairColorTxtBx.Name = "HairColorTxtBx";
             this.HairColorTxtBx.Size = new System.Drawing.Size(222, 24);
             this.HairColorTxtBx.TabIndex = 4;
@@ -90,7 +81,7 @@ namespace DNDCharaterDBApp
             // EyeColorTxtBx
             // 
             this.EyeColorTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EyeColorTxtBx.Location = new System.Drawing.Point(662, 314);
+            this.EyeColorTxtBx.Location = new System.Drawing.Point(669, 372);
             this.EyeColorTxtBx.Name = "EyeColorTxtBx";
             this.EyeColorTxtBx.Size = new System.Drawing.Size(222, 24);
             this.EyeColorTxtBx.TabIndex = 5;
@@ -98,7 +89,7 @@ namespace DNDCharaterDBApp
             // SkinTypeTxtBx
             // 
             this.SkinTypeTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SkinTypeTxtBx.Location = new System.Drawing.Point(1075, 317);
+            this.SkinTypeTxtBx.Location = new System.Drawing.Point(1082, 373);
             this.SkinTypeTxtBx.Name = "SkinTypeTxtBx";
             this.SkinTypeTxtBx.Size = new System.Drawing.Size(222, 24);
             this.SkinTypeTxtBx.TabIndex = 6;
@@ -106,7 +97,7 @@ namespace DNDCharaterDBApp
             // SkinColorTxtBx
             // 
             this.SkinColorTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SkinColorTxtBx.Location = new System.Drawing.Point(662, 392);
+            this.SkinColorTxtBx.Location = new System.Drawing.Point(669, 471);
             this.SkinColorTxtBx.Name = "SkinColorTxtBx";
             this.SkinColorTxtBx.Size = new System.Drawing.Size(222, 24);
             this.SkinColorTxtBx.TabIndex = 7;
@@ -114,7 +105,7 @@ namespace DNDCharaterDBApp
             // RaceNameTxtBx
             // 
             this.RaceNameTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RaceNameTxtBx.Location = new System.Drawing.Point(1075, 392);
+            this.RaceNameTxtBx.Location = new System.Drawing.Point(1082, 469);
             this.RaceNameTxtBx.Name = "RaceNameTxtBx";
             this.RaceNameTxtBx.Size = new System.Drawing.Size(222, 24);
             this.RaceNameTxtBx.TabIndex = 8;
@@ -123,7 +114,7 @@ namespace DNDCharaterDBApp
             // 
             this.ClassTxtBx.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.ClassTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClassTxtBx.Location = new System.Drawing.Point(662, 229);
+            this.ClassTxtBx.Location = new System.Drawing.Point(669, 281);
             this.ClassTxtBx.Name = "ClassTxtBx";
             this.ClassTxtBx.Size = new System.Drawing.Size(222, 24);
             this.ClassTxtBx.TabIndex = 3;
@@ -132,7 +123,7 @@ namespace DNDCharaterDBApp
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(503, 149);
+            this.label1.Location = new System.Drawing.Point(510, 195);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 18);
             this.label1.TabIndex = 10;
@@ -142,7 +133,7 @@ namespace DNDCharaterDBApp
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(943, 149);
+            this.label2.Location = new System.Drawing.Point(950, 192);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 18);
             this.label2.TabIndex = 11;
@@ -152,7 +143,7 @@ namespace DNDCharaterDBApp
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(943, 230);
+            this.label4.Location = new System.Drawing.Point(950, 283);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 18);
             this.label4.TabIndex = 13;
@@ -162,7 +153,7 @@ namespace DNDCharaterDBApp
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(503, 318);
+            this.label13.Location = new System.Drawing.Point(510, 376);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(86, 18);
             this.label13.TabIndex = 5;
@@ -172,7 +163,7 @@ namespace DNDCharaterDBApp
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(943, 318);
+            this.label6.Location = new System.Drawing.Point(950, 374);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(89, 18);
             this.label6.TabIndex = 15;
@@ -182,7 +173,7 @@ namespace DNDCharaterDBApp
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(503, 393);
+            this.label7.Location = new System.Drawing.Point(510, 472);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(91, 18);
             this.label7.TabIndex = 16;
@@ -192,7 +183,7 @@ namespace DNDCharaterDBApp
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(943, 393);
+            this.label8.Location = new System.Drawing.Point(950, 470);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(102, 18);
             this.label8.TabIndex = 8;
@@ -201,7 +192,7 @@ namespace DNDCharaterDBApp
             // CharacterTxtBx
             // 
             this.CharacterTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CharacterTxtBx.Location = new System.Drawing.Point(662, 149);
+            this.CharacterTxtBx.Location = new System.Drawing.Point(669, 195);
             this.CharacterTxtBx.Name = "CharacterTxtBx";
             this.CharacterTxtBx.Size = new System.Drawing.Size(222, 24);
             this.CharacterTxtBx.TabIndex = 1;
@@ -210,21 +201,11 @@ namespace DNDCharaterDBApp
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(503, 233);
+            this.label9.Location = new System.Drawing.Point(510, 285);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(105, 18);
             this.label9.TabIndex = 19;
             this.label9.Text = "ClassName:";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(659, 479);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(105, 18);
-            this.label10.TabIndex = 20;
-            this.label10.Text = "Description:";
             // 
             // label11
             // 
@@ -239,7 +220,7 @@ namespace DNDCharaterDBApp
             // UpdateBtn
             // 
             this.UpdateBtn.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpdateBtn.Location = new System.Drawing.Point(830, 554);
+            this.UpdateBtn.Location = new System.Drawing.Point(830, 597);
             this.UpdateBtn.Name = "UpdateBtn";
             this.UpdateBtn.Size = new System.Drawing.Size(227, 95);
             this.UpdateBtn.TabIndex = 10;
@@ -248,13 +229,12 @@ namespace DNDCharaterDBApp
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(107, 140);
+            this.label3.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 151);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(322, 34);
+            this.label3.Size = new System.Drawing.Size(457, 43);
             this.label3.TabIndex = 23;
-            this.label3.Text = "Choose a Character";
+            this.label3.Text = "Choose a Character To Update";
             // 
             // UpdateCharacterForm
             // 
@@ -264,7 +244,6 @@ namespace DNDCharaterDBApp
             this.Controls.Add(this.label3);
             this.Controls.Add(this.UpdateBtn);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.CharacterTxtBx);
             this.Controls.Add(this.label8);
@@ -281,7 +260,6 @@ namespace DNDCharaterDBApp
             this.Controls.Add(this.EyeColorTxtBx);
             this.Controls.Add(this.HairColorTxtBx);
             this.Controls.Add(this.HairStyleTxtBx);
-            this.Controls.Add(this.DescriptionTxtBx);
             this.Controls.Add(this.CharacterListBx);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "UpdateCharacterForm";
@@ -296,7 +274,6 @@ namespace DNDCharaterDBApp
         #endregion
 
         private System.Windows.Forms.ListBox CharacterListBx;
-        private System.Windows.Forms.TextBox DescriptionTxtBx;
         private System.Windows.Forms.TextBox HairStyleTxtBx;
         private System.Windows.Forms.TextBox HairColorTxtBx;
         private System.Windows.Forms.TextBox EyeColorTxtBx;
@@ -313,7 +290,6 @@ namespace DNDCharaterDBApp
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox CharacterTxtBx;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button UpdateBtn;
         private System.Windows.Forms.Label label3;
